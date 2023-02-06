81e6ffc3dc5077fe7ee96320e10dd406f4068c56
shiftgithub <mhturanmohammad@gmail.com>
2023-02-06 18:31:00 +0600

    push

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    
+    path('', home, name="home"),
+    path('post-todo/', Post_Todo, name="post/todo"),
+    path('get-todo/', Get_Todo, name="get/todo"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+# from django.shortcuts import render,redirect
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TodoSerializer
+from .models import *
 
-# Create your views here.
+
+@api_view(['GET', 'POST', 'PATCH'])
+def home(request):
+    if request.method == 'GET':
+        return Response({
+            'status': 200,
+            'message': 'Yes! Django rest framework is working !!!',
+            'method_called': 'You called GET method',
+        })
+    elif request.method == 'POST':
+        return Response({
+            'status': 200,
+            'message': 'Yes! Django rest framework is working !!!',
+            'method_called': 'You called POST method',
+        })
+    elif request.method == 'PATCH':
+        return Response({
+            'status': 200,
+            'message': 'Yes! Django rest framework is working !!!',
+            'method_called': 'You called PATCH method',
+        })
+    else:
+        return Response({
+            'status': 400,
+            'message': 'Yes! Django rest framework is working !!!',
+            'method_called': 'You called invalid method',
+        })
+
+# @api_view(['GET'])
+# def get_home(request):
+#     return Response({
+#         'status': 200,
+#         'message': 'Yes! Django rest framework is working !!!'
+#     })
+
+
+@api_view(['GET'])
+def Get_Todo(request):
+    todo_objs = Todo.objects.all()
+    seriazlier = TodoSerializer(todo_objs, many=True)
+    return Response(
+        {
+            'status': True,
+            'message': 'Todo fetched',
+            'data': seriazlier.data
+        }
+    )
+
+
+@api_view(['POST'])
+def Post_Todo(request):
+    try:
+        data = request.data
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            # print(serializer.data)
+            return Response({
+                'status': True,
+                'message': 'Success Todo Created.',
+                'data': serializer.data
+            })
+        return Response({
+            'status': False,
+            'message': 'Invalid data',
+            'data': serializer.errors
+        })
+    except Exception as e:
+        print(e)
+    return Response({
+        'status': False,
+        'message': 'something went worng',
+    })
